c4602facedd60c977c47f205074ae7c0275fa303
Bilal-EZ-ZAIM <143717264+Bilal-EZ-ZAIM@users.noreply.github.com>
2023-09-15 08:46:04 +0100

    Update challenge4.c

--- challenge4.c
+++ challenge4.c
@@ -17,15 +17,17 @@ void premierFonction()
 int main()
 {
     premierFonction();
-    float a, b, c, somme, moyenne;
+    float a, b, c , d , somme, moyenne;
     printf("Ecrevez le numéro a ");
     scanf("%f", &a);
     printf("Ecrevez le numéro b ");
     scanf("%f", &b);
     printf("Ecrevez le numéro c ");
     scanf("%f", &c);
+    printf("Ecrevez le numéro d ");
+    scanf("%f", &d);
 
-    somme = a + b + c;
+    somme = a + b + c , d;
     moyenne = somme / 3;
     printf("la somme de  %.2f + %.2f + %.2f = %.2f \n", a, b , c, somme);
     printf("la moyenne de  %.2f / 3 = %.2f \n", somme , moyenne);
